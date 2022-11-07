7857886dcb4df99a70b5640bf837035a10125fc9
Tangui Le Pense <29804907+tanguilp@users.noreply.github.com>
2022-11-07 19:59:05 +0300

    Update for wax 0.6.0

--- lib/wax_fido_test_suite_server_web/endpoint.ex
+++ lib/wax_fido_test_suite_server_web/endpoint.ex
@@ -1,10 +1,6 @@
 defmodule WaxFidoTestSuiteServerWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :wax_fido_test_suite_server
 
-  socket "/socket", WaxFidoTestSuiteServerWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule WaxFidoTestSuiteServer.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:wax_api_rest, "~> 0.3.0"},
+      {:wax_api_rest, "~> 0.4.0"},
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false}
     ]
   end
